ea2bb3e75da6cc8f272ed4a3cacb43f6d3a4b2a6
x4unknown4x <nick776986@gmail.com>
2024-11-18 00:53:16 +0530

    Add files via upload

---
+++ EmailService.java
@@ -0,0 +1,37 @@
+package com.application.vehicleservicemanagement.service.implementation;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+
+    public void sendInvoice(String to, byte[] stream) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setSubject("Vehicle Invoice Service");
+        helper.setText("Please find the invoice attached.");
+        helper.addAttachment("invoice.pdf", new ByteArrayResource(stream));
+        javaMailSender.send(message);
+    }
+}
